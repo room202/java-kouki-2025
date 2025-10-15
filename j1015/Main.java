d6346e3f2fa275b44097767b61309364d978fc57
room202 <yagi@i-seifu.jp>
2025-10-15 11:01:39 +0900

    勇者とマタンゴ作成

---
+++ j1015/Main.java
@@ -0,0 +1,15 @@
+package j1015;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero(); // 勇者が生まれる(インスタンス化)
+        hero.name = "山田太郎";  // 勇者の名前を設定する
+        hero.hp = 100;          // 勇者のHPを設定する
+
+        hero.sleep();
+
+        Matango matango = new Matango();
+        matango.hp = 50;
+        System.out.println("マタンゴのHPは" + matango.hp);
+    }
+}
